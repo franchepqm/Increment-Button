791b8ef9ed0d3d32d40dfac46e108fc5fb44be57
franchepqm_ <74469603+franchepqm@users.noreply.github.com>
2024-07-28 10:07:32 -0300

    Create ImgLocalUpload.js

---
+++ ImgLocalUpload.js
@@ -0,0 +1,13 @@
+function mostrarVistaPrevia(fileInput, visor) {
+    const file = fileInput.files[0];
+    if (file) {
+        const reader = new FileReader();
+        reader.onload = function(e) {
+            visor.src = e.target.result;
+            visor.style.display = 'block';
+        }
+        reader.readAsDataURL(file);
+    } else {
+        visor.style.display = 'none';
+    }
+}
